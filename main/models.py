8af597c7fb3f66e9618ad49d78dea3684ae13dc9
yassinebebba <yassinebebba2@gmail.com>
2021-12-04 21:53:14 +0000

    feat(main): custom user

--- main/models.py
+++ main/models.py
@@ -1,3 +1,62 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import BaseUserManager
+from django.contrib.auth.models import _user_has_perm
+from django.contrib.auth.models import _user_has_module_perms
+from django.utils import timezone
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, phone_number: str, password: str):
+        if not phone_number:
+            return ValueError('User must have a phone number')
+        if not password:
+            return ValueError('User must have a password')
+
+        user = self.model(phone_number=phone_number)
+        user.set_passowrd(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, phone_number: str, password: str):
+        user = self.create_user(phone_number, password)
+        user.is_staff = True
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    class Meta:
+        db_table = 'user'
+
+    phone_number = models.CharField(max_length=15, unique=True, null=False)
+    creation_date = models.DateTimeField(default=timezone.now, null=False)
+    is_active = models.BooleanField(default=False, null=False)
+    is_staff = models.BooleanField(default=False, null=False)
+    is_admin = models.BooleanField(default=False, null=False)
+
+    objects = UserManager()
+
+    USERNAME_FEILD = phone_number
+
+    def has_perm(self, perm: str, obj=None) -> bool:
+        if self.is_active and self.is_admin:
+            return True
+        return _user_has_perm(self, perm, obj)
+
+    def has_perms(self, perms: list[str], obj=None) -> bool:
+        if self.is_active and self.is_admin:
+            return True
+        return all(self.has_perm(perm, obj) for perm in perms)
+
+    def has_module_perms(self, app_label: str) -> bool:
+        if self.is_active and self.is_admin:
+            return True
+        return _user_has_module_perms(self, app_label)
+
+    def __repr__(self):
+        return f'User(phone_number=\'{self.phone_number}\')'
+
+    def __str__(self):
+        return f'{self.phone_number}'
