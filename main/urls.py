ed16d9095063ceac97b8e3d2851a6ce9e7069667
yassinebebba <yassinebebba2@gmail.com>
2021-12-18 17:22:38 +0000

    feat(main/views): ContactsVerificationView

--- main/urls.py
+++ main/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from .views import LoginView
 from .views import OTPVerificationView
 from .views import UserDetailsView
+from .views import ContactsVerificationView
 
 urlpatterns = [
     path('login/', LoginView.as_view()),
     path('verify-otp/', OTPVerificationView.as_view()),
     path('user-details/', UserDetailsView.as_view()),
+    path('check-contacts/', ContactsVerificationView.as_view()),
 ]
